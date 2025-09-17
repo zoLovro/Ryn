3f94da9448ab204dea0fc0b561dbe80253a0be9a
zoLovro <lovrohvalakopar@gmail.com>
2025-09-17 23:48:26 +0200

    Added app icon

--- lwjgl3/src/main/java/io/github/lovrohk/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/io/github/lovrohk/lwjgl3/Lwjgl3Launcher.java
@@ -17,7 +17,7 @@ public class Lwjgl3Launcher {
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
         Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-        configuration.setTitle("LoveLiveRythmGame");
+        configuration.setTitle("Love Live Rythm Game");
         //// Vsync limits the frames per second to what your hardware can display, and helps eliminate
         //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
         configuration.useVsync(true);
@@ -31,7 +31,12 @@ public class Lwjgl3Launcher {
         configuration.setWindowedMode(640, 480);
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
         //// They can also be loaded from the root of assets/ .
-        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        configuration.setWindowIcon(
+            "icons/icon128.png",
+            "icons/icon64.png",
+            "icons/icon32.png",
+            "icons/icon16.png"
+        );
         return configuration;
     }
 }

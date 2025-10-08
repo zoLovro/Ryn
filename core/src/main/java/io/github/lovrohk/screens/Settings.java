38499d3224acce09355ad777c5d14dd2a577c4c3
zoLovro <lovrohvalakopar@gmail.com>
2025-10-08 17:27:49 +0200

    Prepared SettingsManager

--- core/src/main/java/io/github/lovrohk/screens/Settings.java
+++ core/src/main/java/io/github/lovrohk/screens/Settings.java
@@ -2,22 +2,56 @@ package io.github.lovrohk.screens;
 
 import com.badlogic.gdx.Screen;
 import io.github.lovrohk.Main;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
 
 /** First screen of the application. Displayed after the application is created. */
 public class Settings implements Screen {
     protected Main game;
+    protected SpriteBatch batch;
+    protected ShapeRenderer shapeRenderer;
+
+    
+    protected int screenWidth;
+    protected int screenHeight;
+    protected OrthographicCamera camera;
+    protected Viewport viewport;
+
+    protected Texture background; 
 
     public Settings(Main game) {this.game = game;}
 
     @Override
     public void show() {
         // Prepare your screen here.
+        batch = new SpriteBatch();
+        shapeRenderer = new ShapeRenderer();
 
+        // viewport and camera for window resize
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(1920, 1080, camera); //
+        viewport.apply();
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+
+        // bg
+        background = new Texture(Gdx.files.internal("gameImages/settingsBG.jpg"));
+        
+        // useful
+        screenWidth = (int) viewport.getWorldWidth();
+        screenHeight = (int) viewport.getWorldHeight();
     }
 
     @Override
     public void render(float delta) {
         // Draw your screen here. "delta" is the time since last render in seconds.
+
+        // viewport and camera for window resize
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(1920, 1080, camera); //
+        viewport.apply();
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
     }
 
     @Override
---
+++ core/src/main/java/io/github/lovrohk/screensHelpful/SettingsManager.java
@@ -0,0 +1,39 @@
+package io.github.lovrohk.screensHelpful;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Rectangle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SettingsManager {
+    protected Rectangle volumeBar;
+
+    protected int volume;
+    protected int audioOffset;
+    protected float noteSpeed;
+    protected String[] keybinds;
+
+    public SettingsManager(int volume, int audioOffset, float noteSpeed, String[] keybinds) {
+        this.volume = volume;
+        this.audioOffset = audioOffset;
+        this.noteSpeed = noteSpeed;
+        this.keybinds = keybinds;
+    }
+
+
+
+    // getters/setters
+    public void setKeybinds(String[] a) {keybinds = a;}
+    public String[] getKeybinds() {return keybinds;}
+
+    public void setAudioOffset(int a) {audioOffset = a;}
+    public int getAudioOffset() {return audioOffset;}
+
+    public void setNoteSpeed(float a) {noteSpeed = a;}
+    public int getNoteSpeed() {return noteSpeed;}
+
+    public void setVolume(int a) {volume = a;}
+    public int getVolume() {return volume;}
+}
